5b51c9b51a04e7c7a33f159298cccd1674cd6ebb
lukedira02-boop <lukedira02-boop@users.noreply.github.com>
2026-08-01 20:54:23 +0300

    Product Review Form

--- scripts/form.js
+++ scripts/form.js
@@ -1,48 +1,107 @@
-// Product Array Source Data
 const products = [
-  {
-    id: "fc-1888",
-    name: "flux capacitor",
-    averagerating: 4.5
-  },
-  {
-    id: "fc-2050",
-    name: "power laces",
-    averagerating: 4.7
-  },
-  {
-    id: "fs-1987",
-    name: "time circuits",
-    averagerating: 3.5
-  },
-  {
-    id: "ac-2000",
-    name: "low voltage reactor",
-    averagerating: 3.9
-  },
-  {
-    id: "jj-1969",
-    name: "warp equalizer",
-    averagerating: 5.0
-  }
+    {
+        id: "fc-1888",
+        name: "Flux Capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "Power Laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "Time Circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "Low Voltage Reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "Warp Equalizer",
+        averagerating: 5.0
+    }
 ];
 
-// Populate Product Select Options dynamically
+
+// Populate Product Select
+
 document.addEventListener("DOMContentLoaded", () => {
-  const selectElement = document.getElementById("product");
 
-  if (selectElement) {
-    products.forEach(product => {
-      const option = document.createElement("option");
-      option.value = product.id; // Using product's id for the value attribute
-      option.textContent = product.name; // Displaying the product name
-      selectElement.appendChild(option);
-    });
-  }
 
-  // Set footer current year
-  const yearElement = document.getElementById("currentyear");
-  if (yearElement) {
-    yearElement.textContent = new Date().getFullYear();
-  }
+const productSelect = document.querySelector("#product");
+
+
+products.forEach(product => {
+
+    const option = document.createElement("option");
+
+    option.value = product.name;
+
+    option.textContent = product.name;
+
+    productSelect.appendChild(option);
+
 });
+
+
+
+// Footer Year
+
+const year = document.querySelector("#currentyear");
+
+if (year) {
+
+    year.textContent = new Date().getFullYear();
+
+}
+
+
+
+// Last Modified
+
+const modified = document.querySelector("#lastModified");
+
+if (modified) {
+
+    modified.textContent =
+    `Last Modified: ${document.lastModified}`;
+
+}
+
+
+
+});
+
+
+
+// Count review when form is submitted
+
+const form = document.querySelector("form");
+
+
+if(form){
+
+form.addEventListener("submit", () => {
+
+
+let reviews =
+Number(localStorage.getItem("reviewCount-ls")) || 0;
+
+
+reviews++;
+
+
+localStorage.setItem(
+"reviewCount-ls",
+reviews
+);
+
+
+});
+
+
+}
